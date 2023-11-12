e3265dcdd071cafe5cb76825c857f8d3ac5c691e
SantiVentu <santyventu@gmail.com>
2023-11-12 12:36:05 -0300

    cambios inicio fragment

--- app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/fragments/bottombar/InicioFragment.kt
+++ app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/fragments/bottombar/InicioFragment.kt
@@ -1,83 +1,68 @@
 package com.ort.tp_ort_tp3_app_gestordenotas.fragments.bottombar
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
-import com.ort.tp_ort_tp3_app_gestordenotas.EstudianteActivityArgs
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.ort.tp_ort_tp3_app_gestordenotas.EstudianteActivity
 import com.ort.tp_ort_tp3_app_gestordenotas.R
 import com.ort.tp_ort_tp3_app_gestordenotas.entities.Estudiante
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.EstudianteMateria
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Materia
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Usuario
-import com.ort.tp_ort_tp3_app_gestordenotas.fragments.estudiantemateria.EstudianteMateriaFragmentArgs
-import com.ort.tp_ort_tp3_app_gestordenotas.fragments.estudiantes.EstudianteListFragmentDirections
-import kotlinx.coroutines.coroutineScope
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-const val ARG_PARAM1 = "param1"
-const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InicioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InicioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var estudiante: Estudiante;
     private lateinit var v: View;
+    private  lateinit var nombreCompleto: TextView;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        fun newInstance() = InicioFragment()
     }
+
+    private lateinit var viewModel: InicioViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        this.v = inflater.inflate(R.layout.fragment_inicio, container, false);
+        v = inflater.inflate(R.layout.fragment_inicio, container, false)
+        nombreCompleto = v.findViewById(R.id.nombreCompleto);
 
-        return this.v;
+        return v;
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(InicioViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
 
     override fun onStart() {
-        super.onStart();
+        super.onStart()
 
-
-
-        //val u: Usuario = EstudianteActivityArgs.fromBundle(requireArguments()).usuario;
-        //Snackbar.make(this.v, "Estudiante: ${e.getPersona().getNombreCompleto()}", Snackbar.LENGTH_LONG).show();
+        var estudianteActivity: EstudianteActivity = parentFragment?.activity as EstudianteActivity;
+        val idUsuario: String = estudianteActivity.getIdUsuario();
+        //Mandamos a buscar la data del estudiante
+        viewModel.getUsuario(idUsuario);
+        //Observamos la data del estudiante cuando se ejecuta
+        this.getEstudiante()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InicioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InicioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun getEstudiante() {
+        viewModel.estudiante.observe(viewLifecycleOwner, Observer { e ->
+            this.estudiante = e;
+            this.initDataEstudiante(e);
+        });
+    }
+
+
+    private fun initDataEstudiante(e: Estudiante){
+        //this.txtUsuario.text = e?.getUsuario();
+        //this.email.text = e?.getEmail();
+        this.nombreCompleto.text = e?.getPersona()?.getNombreCompleto();
+        //this.dni.text = e?.getPersona()?.getDNI();
     }
 }
