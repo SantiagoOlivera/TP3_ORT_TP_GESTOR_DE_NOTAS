84f9502d2135a75f651d32b3aace15522a1902fe
SantiVentu <santyventu@gmail.com>
2023-11-14 23:31:33 -0300

    genero iniciofragment y su view model nuevamente, cambios menores en el xml de fragment inicio

--- app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/fragments/bottombar/InicioFragment.kt
+++ app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/fragments/bottombar/InicioFragment.kt
@@ -23,15 +23,12 @@ class InicioFragment : Fragment() {
 
     private lateinit var viewModel: InicioViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        v = inflater.inflate(R.layout.fragment_inicio, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        v = inflater.inflate(R.layout.fragment_inicio, container, false);
         nombreCompleto = v.findViewById(R.id.nombreCompleto);
-
         return v;
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
