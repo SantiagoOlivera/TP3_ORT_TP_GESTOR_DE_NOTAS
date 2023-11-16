f33143cae3b45fac10a4c9c7a62d30a6518f16e6
Santiago <santykantor@gmail.com>
2023-11-16 17:59:35 -0300

    intento de cambio de notas de los estudiantemateria

--- app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/adapters/EstudianteMateriaAdapter.kt
+++ app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/adapters/EstudianteMateriaAdapter.kt
@@ -147,23 +147,21 @@ class EstudianteMateriaAdapter(
                              for (d in documentos) {
                                  val datos = d.data
 
-                                     val nota1 = datos["notaParcial1"].toString()?.toIntOrNull() ?: 0
-                                     val nota2 = datos["notaParcial2"].toString()?.toIntOrNull() ?: 0
+                                 val nota1 = datos["notaParcial1"].toString()?.toIntOrNull() ?: 0
+                                 val nota2 = datos["notaParcial2"].toString()?.toIntOrNull() ?: 0
 
                                  Log.d("EstudianteMateriaAdapter", "nota 1: $nota1  nota 2: $nota2")
 
-                                     if (nota1 != null && nota2 != null) {
-                                         holder.itemView.setOnClickListener {
-                                             mostrarMenuEmergente(
-                                                 holder.itemView.context,
-                                                 holder.itemView,
-                                                 nota1,
-                                                 nota2
-                                             )
-                                         }
-                                     } else {
-                                         Log.e("EstudianteMateriaAdapter", "nota1/nota2 da nulo o materia incorrecta")
+                                 if (nota1 != null && nota2 != null) {
+                                     holder.itemView.setOnClickListener {
+                                         mostrarMenuEmergente(
+                                             holder.itemView.context,
+                                             holder.itemView,
+                                             nota1,
+                                             nota2
+                                         )
                                      }
+                                 }
                              }
                          }
                          .addOnFailureListener { e ->
