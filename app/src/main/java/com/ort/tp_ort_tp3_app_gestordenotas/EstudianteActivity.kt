3e0b5bf7d36cfd76a8a6c6324ae2db66f38baaab
SantiVentu <santyventu@gmail.com>
2023-11-16 15:06:13 -0300

    borre dependencias y funciones comentadas que no servian

--- app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/EstudianteActivity.kt
+++ app/src/main/java/com/ort/tp_ort_tp3_app_gestordenotas/EstudianteActivity.kt
@@ -1,40 +1,13 @@
 package com.ort.tp_ort_tp3_app_gestordenotas
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.google.android.gms.common.internal.safeparcel.SafeParcelable
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.ort.tp_ort_tp3_app_gestordenotas.adapters.ViewPagerAdapter
-import com.ort.tp_ort_tp3_app_gestordenotas.databinding.ActivityEstudianteBinding
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.AnioMateria
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.EstadoMateria
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Estudiante
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.EstudianteMateria
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Materia
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Persona
-import com.ort.tp_ort_tp3_app_gestordenotas.entities.Usuario
 import com.ort.tp_ort_tp3_app_gestordenotas.factories.Factory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import java.io.Serializable
-import java.util.Date
-import kotlin.reflect.safeCast
+
 
 
 class EstudianteActivity : AppCompatActivity() {
@@ -42,13 +15,8 @@ class EstudianteActivity : AppCompatActivity() {
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var v: View
-    private lateinit var personaId: String
-    private lateinit var descripcion: String
-    private lateinit var nombreMateria: String
-    private lateinit var anioMateria: AnioMateria
     private lateinit var factory: Factory;
     private lateinit var idUsuario: String;
-    private lateinit var binding: ActivityEstudianteBinding
 
 
 
@@ -76,8 +44,6 @@ class EstudianteActivity : AppCompatActivity() {
 
         //View
         this.v = findViewById(android.R.id.content);
-
-
         this.navHostFragment =
             supportFragmentManager.findFragmentById(R.id.estudiante_navgraph) as NavHostFragment;
 
@@ -88,55 +54,12 @@ class EstudianteActivity : AppCompatActivity() {
 
 
         this.setIdUsuario();
-
-       /* var usuario: String? = intent.getStringExtra("usuario") ?: "";
-        var email: String? = intent.getStringExtra("email") ?: "";
-        var password: String? = intent.getStringExtra("password") ?: "";
-        this.personaId = intent.getStringExtra("idPersona") ?: "";
-
-        //aca
-        if (!usuario.isNullOrBlank() && !email.isNullOrBlank() && !password.isNullOrBlank() && !personaId.isNullOrBlank()) {
-            this.estudiante = Estudiante(
-                usuario.toString(),
-                email.toString(),
-                password.toString(),
-                personaId.toString()
-            );
-        } else {
-            Log.e("Error", "campo invalido")
-        }*/
-
     }
 
 
-        }
-        /*private suspend fun getMateria(idMateria: String): Materia? {
-            var ret: Materia? = null;
-            val db = Firebase.firestore;
 
-            var materia: DocumentSnapshot = db.collection("Materias")
-                .document(idMateria)
-                .get()
-                .await();
+}
 
-            if (materia.data != null) {
-
-                var idMateria: String = materia.id;
-                var descripcion: String = materia.get("descripcion") as String;
-                var nombre: String = materia.get("nombre") as String;
-                var anioMateria: AnioMateria =
-                    AnioMateria.entries.get((materia.get("anioMateria") as Number).toInt());
-
-                ret = Materia(idMateria, nombre, descripcion, anioMateria);
-            }
-
-            //.addOnSuccessListener { materia ->
-            //var m: Materia = this.getMateria(materia);
-            //this.estudiante.agregarEstudianteMateria(em);
-            //}
-
-            return ret;
-        }*/
 
 
 
